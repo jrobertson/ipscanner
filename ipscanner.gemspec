e0f784527a7bf7f323d03a1215d03b317ead51ab
Your Name <james@r0bertson.co.uk>
2011-12-09 15:31:35 +0000

    uses the basic socket instead of net/ping

--- ipscanner.gemspec
+++ ipscanner.gemspec
@@ -1,7 +1,8 @@
 Gem::Specification.new do |s|
   s.name = 'ipscanner'
-  s.version = '0.1.3'
+  s.version = '0.1.4'
   s.summary = 'ipscanner'
+  s.authors = ['James Robertson']
   s.description = "Ping every address on a IPV4 Subnet e.g. 192.168.1.x"
   s.files = Dir['lib/**/*.rb']
   s.add_dependency('net-ping')
--- lib/ipscanner.rb
+++ lib/ipscanner.rb
@@ -2,20 +2,20 @@
 
 # file: ipscanner.rb
 
-require 'net/ping'
+require 'socket'
 
 class IPScanner
 
   def initialize(ip_base='192.168.1.')
     @ip_base = ip_base
     @a = []
-    threads = (1..254).map{|i| Thread.new {ip = ip_base + i.to_s; @a << i if Net::Ping::TCP.new(ip, 'http').ping?}}
+    threads = (1..254).map{|i| Thread.new {ip = ip_base + i.to_s; @a << i if pingecho(ip, 1)}}
     threads.join
   end
   
   def self.run(ip_base='192.168.1.')
     a = []
-    threads = (1..254).map{|i| Thread.new {ip = ip_base + i.to_s; a << i if Net::Ping::TCP.new(ip, 'http').ping?}}
+    threads = (1..254).map{|i| Thread.new {ip = ip_base + i.to_s; a << i if pingecho(ip_base + i.to_s, 1) }}
     threads.join
     a.sort.map{|x| ip_base + x.to_s}
   end
@@ -25,4 +25,19 @@ class IPScanner
   def to_a()
     @a.sort.map{|x| @ip_base + x.to_s}        
   end
+  
+  def self.pingecho(host, timeout=5, service="echo")
+    begin
+      timeout(timeout) do
+	s = TCPSocket.new(host, service)
+	s.close
+      end
+    rescue Errno::ECONNREFUSED
+      return true
+    rescue Timeout::Error, StandardError
+      return false
+    end
+    return true
+  end
+  
 end
