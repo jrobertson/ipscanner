b045d221f259147db14de79a61cde29d81b17504
Your Name <james@r0bertson.co.uk>
2011-12-10 11:10:08 +0000

    now supports optional ranges, an optional array of ip addresses, and an optional timeout.

--- ipscanner.gemspec
+++ ipscanner.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'ipscanner'
-  s.version = '0.1.4'
+  s.version = '0.1.5'
   s.summary = 'ipscanner'
   s.authors = ['James Robertson']
   s.description = "Ping every address on a IPV4 Subnet e.g. 192.168.1.x"
--- lib/ipscanner.rb
+++ lib/ipscanner.rb
@@ -5,27 +5,14 @@
 require 'socket'
 
 class IPScanner
-
-  def initialize(ip_base='192.168.1.')
-    @ip_base = ip_base
-    @a = []
-    threads = (1..254).map{|i| Thread.new {ip = ip_base + i.to_s; @a << i if pingecho(ip, 1)}}
-    threads.join
-  end
   
-  def self.run(ip_base='192.168.1.')
+  def self.scan(ip_base='192.168.1.', range=1..254, t=1)
     a = []
-    threads = (1..254).map{|i| Thread.new {ip = ip_base + i.to_s; a << i if pingecho(ip_base + i.to_s, 1) }}
-    threads.join
-    a.sort.map{|x| ip_base + x.to_s}
+    (range).map{|i| Thread.new {a << i if pingecho(ip_base+i.to_s, t) }}.join
+    sleep t + 0.25
+    a.map{|x| ip_base + x.to_s}
   end
-
-  def self.scan(ip_base='192.168.1.') self.run  end
-    
-  def to_a()
-    @a.sort.map{|x| @ip_base + x.to_s}        
-  end
-  
+      
   def self.pingecho(host, timeout=5, service="echo")
     begin
       timeout(timeout) do
